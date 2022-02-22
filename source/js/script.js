03b54c29e4c534443a6a31690b54035f87560c49
Kate <skoropeya@inbox.ru>
2022-02-22 23:54:00 +0300

    стили для планшетной версии

--- source/js/script.js
+++ source/js/script.js
@@ -1,60 +1,70 @@
-const mainNav = document.querySelector(".main-nav");
-const menuToggle = document.querySelector(".main-nav__toggle");
-const menu = document.querySelector(".main-nav__list-box");
-const tariffsList = document.querySelector(".price__tariffs-list");
-const sliderList = document.querySelector(".slider");
-const modalBuy = document.querySelector(".modal-buy");
-const modalSuccess = document.querySelector(".modal-success");
-const closeButtonBuy = modalBuy.querySelector(".close-button");
-const closeButtonSuccess = modalSuccess.querySelector(".close-button");
+const ECS_KEY = 'Escape';
+const mainNav = document.querySelector('.main-nav');
+const menuToggle = document.querySelector('.main-nav__toggle');
+const menu = document.querySelector('.main-nav__list-box');
+const tariffsList = document.querySelector('.price__tariffs-list');
+const sliderList = document.querySelector('.slider');
+const modalBuy = document.querySelector('.modal-buy');
+
+// Закрытие модального окна
+const closeModal = (modalClass) => {
+  const modal = document.querySelector(`.${modalClass}`);
+  const closeButton = modal.querySelector('.close-button');
+
+  // по клавише escape
+  const onKeydown = (evt) => {
+    if (evt.key === ECS_KEY) {
+      evt.preventDefault();
+      modal.classList.add(`${modalClass}--closed`);
+      modal.classList.remove(`${modalClass}--opened`);
+      document.removeEventListener('keydown', onKeydown);
+    }
+  };
+  // по клику на пустую область модального окна
+  const onModalClick = (evt) => {
+    if (!evt.target.closest(`.${modalClass}__wrapper`)) {
+      modal.classList.add(`${modalClass}--closed`);
+      modal.classList.remove(`${modalClass}--opened`);
+    }
+  };
+  // по кнопке закрытия
+  const onButtonClick = (evt) => {
+    evt.preventDefault();
+    modal.classList.add(`${modalClass}--closed`);
+    modal.classList.remove(`${modalClass}--opened`);
+  };
+
+  document.addEventListener('keydown', onKeydown);
+  modal.addEventListener('click', onModalClick);
+  closeButton.addEventListener('click', onButtonClick);
+};
+
+closeModal('modal-buy');
+closeModal('modal-success');
 
 // Открыть или скрыть меню
-if (mainNav.classList.contains("main-nav--no-js")) {
-  mainNav.classList.remove("main-nav--no-js");
+if (mainNav.classList.contains('main-nav--no-js')) {
+  mainNav.classList.remove('main-nav--no-js');
 }
 
 const onClickMenuToggle = (evt) => {
   evt.preventDefault();
-  menuToggle.classList.toggle("main-nav__toggle--closed");
-  menuToggle.classList.toggle("main-nav__toggle--opened");
-  menu.classList.toggle("main-nav__list-box--closed");
-  menu.classList.toggle("main-nav__list-box--opened");
+  menuToggle.classList.toggle('main-nav__toggle--closed');
+  menuToggle.classList.toggle('main-nav__toggle--opened');
+  menu.classList.toggle('main-nav__list-box--closed');
+  menu.classList.toggle('main-nav__list-box--opened');
 }
 
-menuToggle.addEventListener("click", onClickMenuToggle);
+menuToggle.addEventListener('click', onClickMenuToggle);
 
-// Нажатие на кнопку "Купить тур сейчас" открывает модальное окно
+// Нажатие на кнопку 'Купить тур сейчас' открывает модальное окно
 function buyTour (evt) {
-  if (evt.target.matches("a.button--buy")) {
+  if (evt.target.matches('a.button--buy')) {
     evt.preventDefault();
-    modalBuy.classList.remove("modal-buy--closed");
+    modalBuy.classList.remove('modal-buy--closed');
+    modalBuy.classList.add('modal-buy--opened');
   }
 }
 
 tariffsList.addEventListener('click', buyTour);
 sliderList.addEventListener('click', buyTour);
-
-// Закрытие модального окна
-// по клавише escape
-const onKeydown = (evt) => {
-  if (evt.key === 'Escape') {
-    evt.preventDefault();
-    modalBuy.classList.add("modal-buy--closed");
-    document.removeEventListener('keydown', onKeydown);
-  }
-};
-// по клику на пустую область модального окна
-const onModalClick = (evt) => {
-  if (!evt.target.closest(".modal-buy__wrapper")) {
-    modalBuy.classList.add("modal-buy--closed");
-  }
-};
-// по кнопке закрытия
-const onButtonClick = (evt) => {
-  evt.preventDefault();
-  modalBuy.classList.add("modal-buy--closed");
-};
-
-document.addEventListener('keydown', onKeydown);
-modalBuy.addEventListener('click', onModalClick);
-closeButtonBuy.addEventListener('click', onButtonClick);
